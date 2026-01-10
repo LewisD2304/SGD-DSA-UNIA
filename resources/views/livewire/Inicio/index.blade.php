ede753d701fa7bc1d137bf8f76710f2b2d2b798d
Lewis Diaz <diazdick943@gmail.com>
2026-01-10 02:03:20 -0500

    test2

--- resources/views/livewire/Inicio/index.blade.php
+++ resources/views/livewire/Inicio/index.blade.php
@@ -1,19 +1,12 @@
-<div>
-    @section('breadcrumb')
+@section('breadcrumb')
     <x-breadcrumb titulo="Inicio">
-        <x-breadcrumb.item titulo="Dashboard" />
+        <x-breadcrumb.item titulo="Inicio"/>
     </x-breadcrumb>
-    @endsection
+@endsection
 
-    <div class="row g-5 g-xl-10 mb-5 mb-xl-10">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <h1>Bienvenido al Sistema de Gestión Documental</h1>
-                    <p class="text-muted">Selecciona una opción del menú para comenzar.</p>
-                </div>
-            </div>
-        </div>
+<div>
+    <div class="alert alert-info">
+        <h5>Bienvenido al Sistema de Gestión Documental</h5>
+        <p>Selecciona una opción del menú para comenzar.</p>
     </div>
-
 </div>
