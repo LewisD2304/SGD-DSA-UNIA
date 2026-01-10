67ce938794bc34528e7e35deba5141f6c4a833fd
Lewis Diaz <diazdick943@gmail.com>
2026-01-10 02:11:17 -0500

    index

---
+++ resources/views/livewire/inicio/index.blade.php
@@ -0,0 +1,13 @@
+@section('breadcrumb')
+    <x-breadcrumb titulo="Inicio">
+        <x-breadcrumb.item titulo="Inicio"/>
+    </x-breadcrumb>
+@endsection
+
+<div class="container-fluid">
+    <div class="row mb-3">
+        <div class="col-lg-12">
+            <h1>Bienvenido al Sistema de Gestión Documental</h1>
+        </div>
+    </div>
+</div>
