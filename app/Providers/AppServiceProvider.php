66b1b5e8a38eece204e922d7366b8111f3259f6e
Lewis Diaz <diazdick943@gmail.com>
2026-01-10 00:26:46 -0500

    configuracion despliegue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,9 @@ namespace App\Providers;
 use App\Models\Usuario;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,11 +19,13 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
+
     public function boot(): void
     {
+        if (app()->environment('production') || env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Auth::provider('eloquent', function ($app, array $config) {
             return new \Illuminate\Auth\EloquentUserProvider($app['hash'], Usuario::class);
         });
