a9cd95bf8b63366cef50e5de12f30bf90dcefda0
Lewis Diaz <diazdick943@gmail.com>
2026-01-17 00:42:28 -0500

    doc sol6

--- resources/views/livewire/Documentos/Documento/index.blade.php
+++ resources/views/livewire/Documentos/Documento/index.blade.php
@@ -7,12 +7,12 @@
 
     <livewire:components.Documentos.documento.tabla />
 
-    @include('livewire.components.Documentos.Documento.modal-documento')
-    @include('livewire.components.Documentos.Documento.modal-detalle-documento')
-    @include('livewire.components.Documentos.Documento.modal-eliminar-documento')
-    @include('livewire.components.Documentos.Documento.modal-anular-documento')
-    @include('livewire.components.Documentos.Documento.modal-derivar-documento')
-    @include('livewire.components.Documentos.Documento.modal-rectificar-documento')
-    @include('livewire.components.Documentos.Documento.modal-responder-documento')
-    @include('livewire.components.Documentos.Documento.modal-observacion-documento')
+    @include('livewire.components.Documentos.documento.modal-documento')
+    @include('livewire.components.Documentos.documento.modal-detalle-documento')
+    @include('livewire.components.Documentos.documento.modal-eliminar-documento')
+    @include('livewire.components.Documentos.documento.modal-anular-documento')
+    @include('livewire.components.Documentos.documento.modal-derivar-documento')
+    @include('livewire.components.Documentos.documento.modal-rectificar-documento')
+    @include('livewire.components.Documentos.documento.modal-responder-documento')
+    @include('livewire.components.Documentos.documento.modal-observacion-documento')
 </div>
