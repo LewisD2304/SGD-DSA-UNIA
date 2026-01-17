92d41f9c3bb7b775d13430e16ff4162277a771bf
Lewis Diaz <diazdick943@gmail.com>
2026-01-17 00:37:58 -0500

    doc sol5

--- resources/views/livewire/components/Documentos/documento/tabla.blade.php
+++ resources/views/livewire/components/Documentos/documento/tabla.blade.php
@@ -367,6 +367,6 @@
         </div>
     </div>
 
-    @include('livewire.components.Documentos.Documento.modal-observacion-documento')
+    @include('livewire.components.Documentos.documento.modal-observacion-documento')
 
 </div>
